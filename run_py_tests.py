f33101ae4c9e4a5531d10123dee7e9991c853106
TigerJlx <2274701670@qq.com>
2018-12-12 10:59:34 +0800

    get baidu

--- run_py_tests.py
+++ run_py_tests.py
@@ -3,27 +3,16 @@ from selenium import webdriver
 
 
 class TestClass(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.base_url = "http://www.baidu.com"
 
-	def setUp(self):
-		self.driver = webdriver.Chrome()
-		self.base_url = "http://www.testclass.net"
+    def tearDown(self):
+        time.sleep(5)
+        self.driver.quit()
 
-	def tearDown(self):
-		time.sleep(2)
-		self.driver.quit()
-
-	def test_case(self):
-		self.driver.get(self.base_url)
-		search_input = self.driver.find_element_by_name("q")
-		search_input.send_keys("selenium")
-		search_input.submit()
-
-	def test_case2(self):
-		self.driver.get(self.base_url)
-		search_input = self.driver.find_element_by_name("q")
-		search_input.send_keys("jenkins")
-		search_input.submit()
-
+    def test_case(self):
+        self.driver.get(self.base_url)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
