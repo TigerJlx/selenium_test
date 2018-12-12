0c4b9322372a91a32b3f9e9d95423c0b1e74c8e9
TigerJlx <2274701670@qq.com>
2018-12-12 12:04:00 +0800

    Update run_py_tests.py

--- run_py_tests.py
+++ run_py_tests.py
@@ -20,7 +20,7 @@ class TestClass(unittest.TestCase):
 
     def test_case2(self):
         self.driver.get(self.base_url)
-        self.driver.find_element(By.ID, "email").send_keys("xxxx")
+        self.driver.find_element(By.ID, "email").send_keys("qqqqqqqq")
         self.driver.find_element(By.ID, "password").send_keys("666")
         time.sleep(2)
         self.driver.find_element(By.ID, "submit").click()
