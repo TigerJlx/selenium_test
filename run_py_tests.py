ea5964b85dbb99bd5d9bc6565f7842593c50768f
TigerJlx <2274701670@qq.com>
2018-12-12 13:58:01 +0800

    111

--- run_py_tests.py
+++ run_py_tests.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 class TestClass(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
-        self.base_url = "http://127.0.0.1:500/auth/login"
+        self.base_url = "http://127.0.0.1:5000/auth/login"
 
     def tearDown(self):
         time.sleep(6)
@@ -13,6 +13,7 @@ class TestClass(unittest.TestCase):
 
     def test_case(self):
         self.driver.get(self.base_url)
+        time.sleep(2)
         self.driver.find_element(By.ID, "email").send_keys("2274701670@qq.com")
         self.driver.find_element(By.ID, "password").send_keys("123456")
         time.sleep(2)
@@ -20,6 +21,7 @@ class TestClass(unittest.TestCase):
 
     def test_case2(self):
         self.driver.get(self.base_url)
+        time.sleep(2)
         self.driver.find_element(By.ID, "email").send_keys("qqqqqqqq")
         self.driver.find_element(By.ID, "password").send_keys("666")
         time.sleep(2)
